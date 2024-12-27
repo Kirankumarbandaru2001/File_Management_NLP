ee27aa6180c537738236c315671841af08418c5d
Kirankumarbandaru2001 <Kirankumarbandaru2001@gmail.com>
2024-12-27 13:04:13 +0530

    Added parsing functionality

--- backend/app/routes/routes.py
+++ backend/app/routes/routes.py
@@ -1,9 +1,13 @@
+import os
+
 import boto3
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 from sqlalchemy.orm import Session
 from app.db.connection import get_db
 from app.db.models.document import Document
 from app.config.settings import S3_BUCKET_NAME, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+from app.services.nlp_service import parse_document
 
 router = APIRouter()
 
@@ -22,7 +26,14 @@ async def create_document(title:str,file: UploadFile=File(...),db:Session=Depend
         supported_types = {"application/pdf", "text/csv", "application/vnd.ms-powerpoint"}
         if file.content_type not in supported_types:
             raise HTTPException(status_code=400, detail="Unsupported dile type.")
-        
+
+        # save the file locally temporarily
+        temp_file_path = f"/tmp/{file.filename}"
+        with open(temp_file_path,"wb") as temp_file:
+            temp_file.write(await file.read())
+
+        # Parse document
+        parsed_data = parse_document(temp_file_path)
 
         # Save the document to AWS S3
         s3.put_object(Bucket=S3_BUCKET_NAME, Key=file.filename, Body=file_content)
@@ -32,8 +43,11 @@ async def create_document(title:str,file: UploadFile=File(...),db:Session=Depend
         db.add(document)
         db.commit()
         db.refresh(document)
+
+        # Cleanup temporary file
+        os.remove(temp_file_path)
         
-        return {"id":document.id,"message":"Document created sucesfully"}
+        return {"id":document.id,"message":"Document created successfully"}
 
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occured: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
